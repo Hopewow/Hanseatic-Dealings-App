33a83c1e3cc5c4a4f46c6483ec1aa6754e53effd
Hopewow <43467655+Hopewow@users.noreply.github.com>
2023-01-12 09:16:15 +0100

    Implemented more map buttons and viewmodel for map.

---
+++ Hanseatic Dealings App/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanseatic_Dealings_App.NewFolder
+{
+    class MainViewModel
+    {
+    }
+}
